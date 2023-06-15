d38d4a2780f386913a24c4fa36704613183c9fef
Helena <helena@klarr.se>
2023-06-15 13:24:12 +0200

    added explanation for functions etc

--- script.js
+++ script.js
@@ -54,9 +54,11 @@ let score = 0;
 let time = 10;
 
 // 5.
+// set focus on input element from start
 text.focus();
 
 // 1.
+// generate random word from out words array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
@@ -64,6 +66,7 @@ function getRandomWord() {
 //console.log(getRandomWord());
 
 // 2.
+// add the generated word to the DOM so we can see it in the browser
 function addWordToDOM() {
   randomWord = getRandomWord();
   word.innerHTML = randomWord;
@@ -71,15 +74,19 @@ function addWordToDOM() {
 }
 
 // 4.
+// update the score if the word is correct
 function updateScore() {
   score++;
   scoreEl.innerHTML = score;
 }
 
 // 6.
+// create a timeinterval, 1000 is in milli seconds
 const timeInterval = setInterval(updateTime, 1000);
 
 // 7.
+// update the time and count down from initial time. When time hits 0 it shoudl be game over
+// therefor we need to clear the timeinterval and then call for gameOver()function
 function updateTime() {
   time--;
 
@@ -91,25 +98,35 @@ function updateTime() {
   }
 }
 
+// when time hits 0 its game over. We create some new HTML elements and also a button that reloads the game
+// if we click it.
 function gameOver() {
   endgameEl.innerHTML = `<h1>Time ran out!</h1> <p>Your final score is: ${score}</p> <button onClick="location.reload()">Reload</button>`;
 
   endgameEl.style.display = "flex";
 }
 
+// call for function to add word from array to DOM
 addWordToDOM();
 
 // 3.
+// eventlisterner for our input field. We make the event listen to the input then we use the event parameter to get hold
+// of the exact value that the user has typed
 text.addEventListener("input", (event) => {
   const insertedText = event.target.value;
 
+  // check if typed word is exactly the same as the random word
   if (insertedText === randomWord) {
+    // if that is true add a new word to the DOM
     addWordToDOM();
 
+    // and update the score
     updateScore();
 
+    // we need to clear the input field otherwise we have to manually remove the old typed word
     event.target.value = "";
 
+    // increase time based on what setting user chose
     if (difficulty === "hard") {
       time += 2;
     } else if (difficulty === "medium") {
@@ -118,10 +135,13 @@ text.addEventListener("input", (event) => {
       time += 5;
     }
 
+    // update the time
     updateTime();
   }
 });
 
+// eventlistener for our settings. This will listen to a change in the select options, again we grab the
+// exact value by using the event parameter
 settingsForm.addEventListener("change", (event) => {
   difficulty = event.target.value;
 
